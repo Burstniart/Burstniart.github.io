ff838b101be0944744f65f0063d2b649c5e3122c
Alejandro Garcia <v4gr33n@hotmail.com>
2022-03-26 19:26:22 -0600

    Initial template

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+console.log(`I'm working`);
+function openPage(pageName, element, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+    }
+    tablinks = document.getElementsByClassName(`tabLink`);
+    for ( i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = '';
+    }
+    document.getElementById(pageName).style.display = 'block';
+
+    element.style.backgroundColor = color;
+}
+document.getElementById('defaultOpen').onmouseover();
